bd28a211576851df5a0cb6ba5ce7e84315f6812a
jluczak <jluczaksas@gmail.com>
2019-04-01 21:25:20 +0200

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -10,9 +14,32 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    get login_path
+    assert_select "title", full_title("Log in")
+  end
+
+  test "layout links after login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    get users_path
+    assert_select "title", full_title("All users")
+    get user_path(@user)
+    assert_select "title", full_title(@user.name)
+    get edit_user_path(@user)
+    assert_select "title", full_title("Edit user")
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      assert_select 'form[action="/signup"]'
+      assert_select 'form[action="/users"]'
       post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
